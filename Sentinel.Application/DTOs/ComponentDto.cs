30f7b524197489bcde4f1f3dc88c51e46f129c45
Eren <erenbalta342115@gmail.com>
2026-03-22 01:21:25 +0300

    Automapper / Dependency Injection integration

---
+++ Sentinel.Application/DTOs/ComponentDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentinel.Application.DTOs
+{
+    public record ComponentDto(Guid Id, string Name, string Version, string Purl, bool IsTransitive, string LicenseName);
+}
---
+++ Sentinel.Application/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Scrutor;
+
+namespace Sentinel.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // 1. AutoMapper Kaydı
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // 2. Scrutor ile Akıllı Tarama (Smart Registration)
+            services.Scan(selector => selector
+                .FromAssemblies(typeof(DependencyInjection).Assembly) // Mevcut assembly'i (Application) tara
+                .AddClasses(classes => classes.Where(type =>
+                    type.Name.EndsWith("Service") ||
+                    type.Name.EndsWith("Strategy"))) // Hem Servisleri hem Stratejileri bul
+                .AsImplementedInterfaces() // IScannerService, IParserStrategy gibi arayüzlerle eşleştir
+                .WithScopedLifetime()); // İstek bazlı (Scoped) ömür biç
+
+            return services;
+        }
+    }
+}
